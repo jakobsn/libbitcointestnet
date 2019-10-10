c45beb7f141824e3cb6aad208ce0ffd186994b73
jakobsn <jakob_sn_@hotmail.com>
2019-10-10 16:45:32 +0200

    beautuful

--- address.sh
+++ address.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 x=1
+echo current directory is $PWD
+ls -l bx-linux-x64-qrcode
 echo "address, seed, priv key, pup key" >> wallet.txt 
 while [ true ]
 do
 	seed=$(bx seed)
-	private_key=$(echo $seed | bx ec-new)
-	public_key=$(echo $private_key | bx ec-to-public)
-	address=$(echo $public_key | bx ec-to-address)
+	private_key=$(echo $seed | bx ec-new --config bx.cfg)
+	public_key=$(echo $private_key | bx ec-to-public --config bx.cfg)
+	address=$(echo $public_key | bx ec-to-address --config bx.cfg)
 
 	if [[ $address == *"11" ]]; then
+
 		echo $address >> wallet.txt
 		echo $seed >> wallet.txt
 		echo $private_key >> wallet.txt
---
+++ test.sh
@@ -0,0 +1,8 @@
+word="mdsa11"
+
+if [[ $word == "m"*"11" ]] || [[ $word == "c"*"11" ]] 
+then
+  echo "aids"
+fi
+echo "echo"
+
